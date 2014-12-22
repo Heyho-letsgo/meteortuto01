26a3603990d04d9b9855884d61b789c129019a06
André Baudin-Laurencin <abaudin@gmail.com>
2014-12-22 12:27:01 +0100

    12.4 Passing Arguments

--- leaderboard.js
+++ leaderboard.js
@@ -50,28 +50,26 @@ if (Meteor.isClient) {
         'submit form': function (theEvent, theTemplate) {
             theEvent.preventDefault();
             var playerNameVar = theTemplate.find('#playerName').value;
+            Meteor.call('insertPlayerData');
+        }
+    });
+}
+
+
+if (Meteor.isServer) {
+    console.log(PlayersList.find().fetch());
+    Meteor.publish('thePlayers', function () {
+        var currentUserId = this.userId;
+        return PlayersList.find({createdBy: currentUserId});
+    });
+    Meteor.methods({
+        'insertPlayerData': function (playerNameVar){
             var currentUserId = Meteor.userId();
             PlayersList.insert({
                 name: playerNameVar,
                 score: 0,
                 createdBy: currentUserId
-            });
-        }
-    });
-            Meteor.call('executeLogStatement');
-
-}
-
-
-if (Meteor.isServer) {
-   console.log(PlayersList.find().fetch());
-    Meteor.publish('thePlayers', function(){
-        var currentUserId = this.userId;
-        return PlayersList.find({createdBy: currentUserId});
-    });
-    Meteor.methods({
-        'executeLogStatement' : function(){
-            console.log("You submitted the form");
+            })
         }
     });
 
